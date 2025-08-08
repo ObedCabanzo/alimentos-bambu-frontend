f223c5ba3a93070bc8bb78bed5bf779ff8d7945b
Obed Cabanzo <obedc132@gmail.com>
2025-08-07 19:19:29 -0500

    fix: pauzé navbar link removed

--- bambu-app/src/config/data_navbar.js
+++ bambu-app/src/config/data_navbar.js
@@ -3,6 +3,5 @@ export const navLinks = [
   { href: "/productos", label: "Nuestros Productos" },
   { href: "/bital", label: "Nuestra Marca" },
   { href: "/ingenieria", label: "Ingeniería Bambú" },
-  { href: "/contactanos", label: "Contáctanos" },
-  { href: "/pauze", label: "Pauzë" },
+  { href: "/contactanos", label: "Contáctanos" }
 ];
